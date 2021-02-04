bb7efb5921f9367df32500691a8daae34a453ca6
Arthur Grigoletto <arthur.grigoletto.lima@gmail.com>
2021-02-04 10:04:00 -0300

    Fix errors

--- codepix/infrastructure/repository/pix.go
+++ codepix/infrastructure/repository/pix.go
@@ -10,62 +10,56 @@ type PixKeyRepositoryDb struct {
 	Db *gorm.DB
 }
 
-func (pixKeyRepository PixKeyRepositoryDb) AddBank(bank *model.Bank) error {
-	err := pixKeyRepository.Db.Create(bank).Error
+func (r PixKeyRepositoryDb) AddBank(bank *model.Bank) error {
+	err := r.Db.Create(bank).Error
 	if err != nil {
 		return err
 	}
-
 	return nil
 }
 
-func (pixKeyRepository PixKeyRepositoryDb) AddAccount(account *model.Account) error {
-	err := pixKeyRepository.Db.Create(account).Error
+func (r PixKeyRepositoryDb) AddAccount(account *model.Account) error {
+	err := r.Db.Create(account).Error
 	if err != nil {
 		return err
 	}
-
 	return nil
 }
 
-func (pixKeyRepository PixKeyRepositoryDb) RegisterKey(pixKey *model.PixKey) (*model.PixKey, error) {
-	err := pixKeyRepository.Db.Create(pixKey).Error
+func (r PixKeyRepositoryDb) RegisterKey(pixKey *model.PixKey) (*model.PixKey, error) {
+	err := r.Db.Create(pixKey).Error
 	if err != nil {
 		return nil, err
 	}
-
 	return pixKey, nil
 }
 
-func (pixKeyRepository PixKeyRepositoryDb) FindKeyByKind(key string, kind string) (*model.PixKey, error) {
+func (r PixKeyRepositoryDb) FindKeyByKind(key string, kind string) (*model.PixKey, error) {
 	var pixKey model.PixKey
-	pixKeyRepository.Db.Preload("Account.Bank").First(&pixKey, "kind = ? and key = ?", kind, key)
+	r.Db.Preload("Account.Bank").First(&pixKey, "kind = ? and key = ?", kind, key)
 
 	if pixKey.ID == "" {
 		return nil, fmt.Errorf("no key was found")
 	}
-
 	return &pixKey, nil
 }
 
-func (pixKeyRepository PixKeyRepositoryDb) FindAccount(id string) (*model.Account, error) {
+func (r PixKeyRepositoryDb) FindAccount(id string) (*model.Account, error) {
 	var account model.Account
-	pixKeyRepository.Db.Preload("Bank").First(&account, "id = ?", id)
+	r.Db.Preload("Bank").First(&account, "id = ?", id)
 
 	if account.ID == "" {
 		return nil, fmt.Errorf("no account found")
 	}
-
 	return &account, nil
 }
 
-func (pixKeyRepository PixKeyRepositoryDb) FindBank(id string) (*model.Bank, error) {
+func (r PixKeyRepositoryDb) FindBank(id string) (*model.Bank, error) {
 	var bank model.Bank
-	pixKeyRepository.Db.First(&bank, "id = ?", id)
+	r.Db.First(&bank, "id = ?", id)
 
 	if bank.ID == "" {
 		return nil, fmt.Errorf("no bank found")
 	}
-
 	return &bank, nil
 }
--- codepix/infrastructure/repository/transaction.go
+++ codepix/infrastructure/repository/transaction.go
@@ -3,35 +3,35 @@ package repository
 import (
 	"fmt"
 	"github.com/arthurgrigoletto/imersao-fullcycle/codepix-go/domain/model"
-	"gorm.io/gorm"
+	"github.com/jinzhu/gorm"
 )
 
 type TransactionRepositoryDb struct {
 	Db *gorm.DB
 }
 
-func (transactionRepository *TransactionRepositoryDb) Register(transaction *model.Transaction) error {
-	err := transactionRepository.Db.Create(transaction).Error
+func (t *TransactionRepositoryDb) Register(transaction *model.Transaction) error {
+	err := t.Db.Create(transaction).Error
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func (transactionRepository *TransactionRepositoryDb) Save(transaction *model.Transaction) error {
-	err := transactionRepository.Db.Save(transaction).Error
+func (t *TransactionRepositoryDb) Save(transaction *model.Transaction) error {
+	err := t.Db.Save(transaction).Error
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func (transactionRepository *TransactionRepositoryDb) Find(id string) (*model.Transaction, error) {
+func (t *TransactionRepositoryDb) Find(id string) (*model.Transaction, error) {
 	var transaction model.Transaction
-	transactionRepository.Db.Preload("AccountFrom.Bank").First(&transaction, "id = ?", id)
+	t.Db.Preload("AccountFrom.Bank").First(&transaction, "id = ?", id)
 
 	if transaction.ID == "" {
-		return nil, fmt.Errorf("no transaction was found")
+		return nil, fmt.Errorf("no key was found")
 	}
 	return &transaction, nil
 }
